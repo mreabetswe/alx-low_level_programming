f949b42546ce49385f35119c27daf303bbf451f3
mreabetswe <mokotoreabetswe123@gmail.com>
2023-04-23 11:58:02 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,13 @@
 int main(void)
 {
 	char low;
-	for (low = 'a';low <= 'z'; low++)
+
+	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 
 	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
-	putchar('\n');
+	putchar ('\n');
 
-	return(0);
+	return (0);
 }
