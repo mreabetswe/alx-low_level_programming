4dafec47fc8c501b9c41ec9acb2c65b85a1db473
mreabetswe <mokotoreabetswe123@gmail.com>
2023-04-24 04:22:22 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,18 @@
  */
 int main(void)
 {
-	int n;
+	int d;
 
-	for (n = '0'; n <= '9'; n++)
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(n);
-		if (n != '9')
+		putchar(d);
+		if (d != '9')
 		{
 			putchar('.');
 			putchar(' ');
 		}
+	}
 		putchar('\n');
 		return (0);
-	}
+	
 }
