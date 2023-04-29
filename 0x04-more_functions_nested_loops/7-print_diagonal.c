80d8a09c882c0d5a93a11c0f6444d1b70d6269b9
mreabetswe <mokotoreabetswe123@gmail.com>
2023-04-28 21:41:56 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal lines according to parameter
+ * @n: The number of times to print diagonal lines
+ * @i: The number of times to print diagonal lines
+ * @j: The number of times to print diagonal lines
+ * Return: empty
+ */
+
+void print_diagonal(int n)
+
+{
+	if  (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i, j
+
+	for (i = 0; i < n; i++);
+	{
+	for (j = 0; j < n; j++);
+	{
+	if (j == i)
+	_putchar('\\'); 
+	else if (j < i)
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
+	}
+}
