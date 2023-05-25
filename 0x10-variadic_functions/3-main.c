e3015bc65d73bc8aac758d0e82a9d105b087246c
mreabetswe <mokotoreabetswe123@gmail.com>
2023-05-25 15:30:26 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+julien@ubuntu:~/0x0f. Variadic functions$ cat 3-main.c
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef _VARIADIC_H
+#define _VARIADIC_H
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+void print_a_char(char *separator, va_list args);
+void print_a_integer(char *separator, va_list args);
+void print_a_float(char *separator, va_list args);
+void print_a_char_ptr(char *separator, va_list args);
+
+#endif            
