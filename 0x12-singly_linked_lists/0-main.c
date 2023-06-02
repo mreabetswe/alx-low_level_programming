280c4192fa2c1821f9c1f25db1843de27414af2f
mreabetswe <reabetswemokoto123@gmail.com>
2023-06-02 16:36:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -2,17 +2,28 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
- */ int main(void) {
+ */ 
+
+int main(void) 
+{
     list_t *head;
     list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
+    list_t hello = {"World", 5};
+    list_t*next_node = NULL;
+    hello.next = next_node;
     head = &hello;
     new = malloc(sizeof(list_t));
+    typedef struct list_s
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+    list_t;
+
     if (new == NULL)
     {
         printf("Error\n");
@@ -23,12 +34,12 @@
     new->next = head;
     head = new;
     n = print_list(head);
-    printf("-> %lu elements\n", n);
+    printf("-> %u elements\n", *int);
     printf("\n");
     free(new->str);
     new->str = NULL;
     n = print_list(head);
-    printf("-> %lu elements\n", n);
+    printf("-> %u elements\n", *int);
     free(new);
     return (0);
 }
