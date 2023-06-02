ee16ebf1a6e88e4953c145738ca43773db9ca757
mreabetswe <reabetswemokoto123@gmail.com>
2023-06-02 14:29:47 -0700

    4-free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,13 +11,13 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head)
+	temp = head;
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
 		free(temp->str);
 		free(temp);
 	}
-
 	free(head);
 }
