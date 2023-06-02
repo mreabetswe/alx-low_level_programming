eb0c05caf91497c3d6e9ca99f747e949feaba247
mreabetswe <reabetswemokoto123@gmail.com>
2023-06-02 14:16:35 -0700

    100-first.c

--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -2,15 +2,19 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
- */ int main(void) {
+ */
+int main(void)
+{
     list_t *head;
     list_t *new;
     list_t hello = {"World", 5, NULL};
     size_t n;
+
     head = &hello;
     new = malloc(sizeof(list_t));
     if (new == NULL)
@@ -22,13 +26,9 @@
     new->len = 5;
     new->next = head;
     head = new;
-    n = print_list(head);
+    n = list_len(head);
     printf("-> %lu elements\n", n);
-    printf("\n");
     free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
     free(new);
     return (0);
 }
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - prints out a function executed before main
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
